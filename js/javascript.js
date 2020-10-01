d4dd9877667d67e1309a4477d5d8d484f16d574c
Xackspel <42944802+Xackspel@users.noreply.github.com>
2020-10-01 23:23:58 +0300

    First project
    Attached first project.

---
+++ js/javascript.js
@@ -0,0 +1,4 @@
+console.log("Hello World!"); //Say hello in console.
+document.getElementById("out").innerHTML = "Hello JS <b>2020</b></b>"; // Greetings on frontpage.
+document.querySelector("h2.header").innerHTML = "60";
+// alert("Welcome on my page!");
